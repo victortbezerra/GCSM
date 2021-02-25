9305461e47a1cad9d7a07ce0f102f7af0520abb4
victor <victor@VICTOR>
2021-02-25 13:31:09 -0300

    The Final Countdown

--- GCSM - Git/src/HelloWorld.java
+++ GCSM - Git/src/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("1 +1 = 2");
-		System.out.println("X86_64");
+		System.out.println("The Final Countdown");
 	}
 }
