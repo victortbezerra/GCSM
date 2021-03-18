f6b7416e0cf6feb8f7f428a8f4427882b46076d3
Victor <victortbezerra@outlook.com>
2021-03-17 22:01:42 -0300

    Delete GCSM - Git directory

--- GCSM - Git/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("The Final Countdown");
-		
-		System.out.println("Dona de mim");
-		System.out.println("99,17%");
-		System.out.println("Breakdown");
-	}
-}
