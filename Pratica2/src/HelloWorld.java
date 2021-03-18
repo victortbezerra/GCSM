79a52f42f0be4d162a972a53cda660fe53be79d2
Victor Teixeira Bezerra <victortbezerra@outlook.com>
2021-03-17 22:21:59 -0300

    Conflit

--- Pratica2/src/HelloWorld.java
+++ Pratica2/src/HelloWorld.java
@@ -1,9 +1,4 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hotfix");
-=======
-		System.out.println("Master");
->>>>>>> refs/heads/master
-	}
 }
